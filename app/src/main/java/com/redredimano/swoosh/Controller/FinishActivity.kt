24443f9d7b11c62fce1d8d8416b6b6b7270e9ccc
Generoso Redimano Jr <gredimano@objectbright.com>
2018-01-23 09:41:52 +0800

    Lesson 8.

--- app/src/main/java/com/redredimano/swoosh/Controller/FinishActivity.kt
+++ app/src/main/java/com/redredimano/swoosh/Controller/FinishActivity.kt
@@ -2,9 +2,9 @@ package com.redredimano.swoosh.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.redredimano.swoosh.Model.Player
 import com.redredimano.swoosh.R
-import com.redredimano.swoosh.Utilities.EXTRA_LEAGUE
-import com.redredimano.swoosh.Utilities.EXTRA_SKILL
+import com.redredimano.swoosh.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_finish.*
 
 class FinishActivity : BaseActivity() {
@@ -13,9 +13,8 @@ class FinishActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
 
-        val league = intent.getStringExtra(EXTRA_LEAGUE)
-        val skill = intent.getStringExtra(EXTRA_SKILL)
+        val player = intent.getParcelableExtra<Player>(EXTRA_PLAYER)
 
-        searchLeaguesText.text = "Looking for $league $skill league near you..."
+        searchLeaguesText.text = "Looking for ${player.league} ${player.skill} league near you..."
     }
 }
